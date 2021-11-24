686b346c695941ebd1f7af3e940d6b9547c98bf4
Begin-Ka <90418283+Begin-Ka@users.noreply.github.com>
2021-11-24 14:25:58 +0300

    form

---
+++ form/index.php
@@ -0,0 +1,10 @@
+<form id="form" action="send.php" method="post">                   
+                        
+    <h2 class="letter-name">Отправить запрос</h2>
+
+    <input type="text" name="name" placeholder="Введите Ваше имя..." required><br>
+    <input type="text" name="email" placeholder="Введите e-mail..." required><br>
+    <input type="text" name="phone" placeholder="Введите номер телефона..."><br>
+    <input class="massage" type="text" name="message" placeholder="Введите текст запроса..."><br>
+    <input type="submit" class="letter-btn" value="Отправить">
+</form>
